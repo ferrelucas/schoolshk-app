0a2cd24e24e675ea9e844d25df3cad21493c31fa
ferrelucas <ferrelucas.silva@gmail.com>
2025-07-15 00:56:34 +0800

    Remove navigation menu - load schools page directly

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
 import Schools from './pages/Schools';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navigation />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Schools />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="App">
+      <main className="main-content">
+        <Schools />
+      </main>
+    </div>
   );
 }
 
